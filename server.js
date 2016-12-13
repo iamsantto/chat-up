b1bb566bcc65c31d2cce2d30959c45127f97e2c9
iamsantto <santhos009@gmail.com>
2016-12-13 20:25:53 +0530

    favicon added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // ---------------- Required Modules ---------------- >>
 var express = require('express');
+var app = express();
 var path = require('path');
 var morgan = require('morgan');
 var favicon = require('serve-favicon');
@@ -7,22 +8,18 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 // ---------------- Varibles ---------------- >>
-var app = express();
 var port = process.env.PORT || 9900;
-
 
 // ---------------- Static Files served at /public ---------------- >>
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // ---------------- Serving Favicon ---------------- >>
-app.use(favicon(__dirname + '/public/images/fav.png'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 // ---------------- Log HTTP Requests ---------------- >>
 app.use(morgan('dev'));
 
-
-// ---------------- Log Server & Port Details ---------------- >>
-console.log('All good !');
+// ---------------- Log Port Details ---------------- >>
 console.log('Server running at ' + port);
 
 // ---------------- Start Server ---------------- >>
